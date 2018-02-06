ed04577964bcd05f9c779794227841f7a3bccf49
jinjingyuan <jinjingyuan@quhuanqian.cn>
2018-02-06 17:40:33 +0800

    Initial Commit

---
+++ EmptyView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmptyView
+//
+//  Created by jy on 2018/2/6.
+//  Copyright © 2018年 M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EmptyViewTests/EmptyViewTests.m
@@ -0,0 +1,39 @@
+//
+//  EmptyViewTests.m
+//  EmptyViewTests
+//
+//  Created by jy on 2018/2/6.
+//  Copyright © 2018年 M. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EmptyViewTests : XCTestCase
+
+@end
+
+@implementation EmptyViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
